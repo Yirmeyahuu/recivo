111afabfbe62fa964630d3bbb7c89f6314f66d03
Jeremiah Pantaras <jeremiahpantaras@gmail.com>
2025-12-23 13:48:52 +0800

    new:seo

--- recivo-web/capacitor.config.ts
+++ recivo-web/capacitor.config.ts
@@ -2,8 +2,22 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.recivo.app',
-  appName: 'recivo',
-  webDir: 'dist'
+  appName: 'Recivo Android',
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https',
+    cleartext: true
+  },
+  plugins: {
+    FirebaseAuthentication: {
+      skipNativeAuth: false,
+      providers: ['google.com'],
+      // Add this for Android
+      android: {
+        providers: ['google.com']
+      }
+    }
+  }
 };
 
 export default config;
